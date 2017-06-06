f6f8fe8c55dfd2b367b0b29e1f96d43703bd391e
Fernando Albernaz <albernazf@outlook.com>
2017-06-06 13:44:04 +1000

    added IoT Hub connection

--- Macaroni.UWP/Macaroni.UWP/MainPage.xaml.cs
+++ Macaroni.UWP/Macaroni.UWP/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using Message = Microsoft.Azure.Devices.Message;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +29,29 @@ namespace Macaroni.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
+        private static DeviceClient deviceClient;
+        private string iotHubUri = "FerTestHub.azure-devices.net";
+        private string deviceKey = "PF2Njw/krzA8l7fLzIgiDTXAmhr5DhppUpvA5aBRqE0=";
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+        }
+
+        public async Task StartCommunication()
+        {
+            while (true)
+            {
+                var receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                //Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
         }
     }
 }
