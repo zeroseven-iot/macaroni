2a7d6df10889efddfcb35c8f7c07432b78987722
Fernando Albernaz <fernando@builttoroam.com>
2017-06-11 15:20:41 +1000

    Configured device to receive messages and added send message test project

---
+++ Macaroni.UWP/Macaroni.SendMessageToDevice/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macaroni.SendMessageToDevice
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=FerTestHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=+wWaCJYd3dYBd37c6Bkm/2/be7Y/xUM7TWeYw7iS9f4=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            Console.WriteLine("Type a message to send and press enter.");
+            var message = Console.ReadLine();
+            SendCloudToDeviceMessageAsync(message).Wait();
+            Console.ReadLine();
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync(string message)
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(message));
+            await serviceClient.SendAsync("Macaroni_01", commandMessage);
+        }
+    }
+}
--- Macaroni.UWP/Macaroni.UWP/MainPage.xaml.cs
+++ Macaroni.UWP/Macaroni.UWP/MainPage.xaml.cs
@@ -17,6 +17,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Client;
+using System.IO.IsolatedStorage;
+using Microsoft.Azure.Devices.Client.Exceptions;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,28 +29,56 @@ namespace Macaroni.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        
-        private static DeviceClient deviceClient;
-        private string iotHubUri = "FerTestHub.azure-devices.net";
-        private string deviceKey = "PF2Njw/krzA8l7fLzIgiDTXAmhr5DhppUpvA5aBRqE0=";
+        static DeviceClient deviceClient;
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=FerTestHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=+wWaCJYd3dYBd37c6Bkm/2/be7Y/xUM7TWeYw7iS9f4=";
+        static string iotHubUri = "FerTestHub.azure-devices.net";
+        string deviceId = "Macaroni_01";
 
         public MainPage()
         {
             this.InitializeComponent();
-
-            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
-
-            StartCommunication();
         }
 
-        public async Task StartCommunication()
+
+        private async Task<string> GetDeviceKey()
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+
+            return device.Authentication.SymmetricKey.PrimaryKey;
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text += "Getting device key" + Environment.NewLine;
+            string deviceKey = await GetDeviceKey();
+            txtOutput.Text += "Device key is: " + deviceKey + Environment.NewLine;
+            
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+
+            txtOutput.Text += "Device client successfully created, waiting for messages" + Environment.NewLine;
+            ReceiveC2dAsync();
+        }
+
+        private async void ReceiveC2dAsync()
         {
             while (true)
             {
-                var receivedMessage = await deviceClient.ReceiveAsync();
+                Microsoft.Azure.Devices.Client.Message receivedMessage = await deviceClient.ReceiveAsync();
                 if (receivedMessage == null) continue;
 
-                //Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                string message = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+
+                txtOutput.Text += "Message received: " + message  + Environment.NewLine;
 
                 await deviceClient.CompleteAsync(receivedMessage);
             }
