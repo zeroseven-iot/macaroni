256c3f58c32529921b4b5fe0447d3b3a46ed558f
Francisco <franciscofeko@gmail.com>
2017-06-19 20:43:49 +1000

    Send message to event hub

--- Macaroni.Ionic/src/pages/home/home.ts
+++ Macaroni.Ionic/src/pages/home/home.ts
@@ -8,6 +8,9 @@ import { ReconnectingWebSocket } from '../../app/helpers/reconnectingwebsocket';
 })
 export class HomePage {
 	public azureSocket:ReconnectingWebSocket;
+
+	private messageStatus:string = "";
+
 	constructor(public navCtrl: NavController) {
 		//this.azureSocket = new ReconnectingWebSocket("wss://<YOUR-IOT-HUB-NAME>.azure-devices.net:443/$iothub/websocket", ["AMQPWSB10"]);
 		this.azureSocket = new ReconnectingWebSocket("ws://echo.websocket.org")
@@ -15,5 +18,35 @@ export class HomePage {
 
 	public sendMessageToAzure() {
 		this.azureSocket.send("This is a test");
+	}
+
+	sendMessageToEventHub() {
+
+		var self = this;
+		// Generate a SAS key with the Signature Generator.: https://github.com/sandrinodimattia/RedDog/releases
+		// Could be provided by a Web API.
+		var sas = "SharedAccessSignature sr=https%3a%2f%2freddogeventhub.servicebus.windows.net%2fmesssage%2fpublishers%2fphone%2fmessages&sig=Luqu%2fZQd6rfhCdGPTZlhMYCVtXM51QWsdSVlc08LGWc%3d&se=1405564221&skn=SenderDevice";
+
+		var serviceNamespace = "customeventhub";
+		var hubName = "iotHub";
+		var deviceName = "phone";
+
+		var xmlHttpRequest = new XMLHttpRequest();
+
+		xmlHttpRequest.open("POST", "https://" + serviceNamespace + ".servicebus.windows.net/" + hubName + "/publishers/" + deviceName + "/messages", true);
+		xmlHttpRequest.setRequestHeader('Content-Type', "application/atom+xml;type=entry;charset=utf-8");
+		xmlHttpRequest.setRequestHeader("Authorization", sas);
+
+		xmlHttpRequest.onreadystatechange = function () {
+			if (this.readyState == 4) {
+
+				if (this.status == 201) {
+					self.messageStatus = "Status: Ready to send";
+				} else {
+					self.messageStatus = "Status: " + this.status.toString();
+				}
+			}
+		};
+    	xmlHttpRequest.send("{ Message: Test message }");
 	}
 }
